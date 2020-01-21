ebf8c58cb386ae454cd5b22b82ddeb2791dcdc75
cpodimas@gmail.com <cpodimas@gmail.com>
2020-01-21 10:59:38 +0300

    v0.0.8

--- src/Endpoint.js
+++ src/Endpoint.js
@@ -54,6 +54,16 @@ export default class Endpoint extends EventEmitter {
             }
             */
 
+            // Produce an error if we don't have the native module
+            if (NativeModules.TeleModule == null) {
+                throw new Error(`react-native-tele: NativeModule.TeleModule is null. To fix this issue try these steps:
+            • Rebuild and re-run the app.
+            • If you are using CocoaPods on iOS, run \`pod install\` in the \`ios\` directory and then rebuild and re-run the app. You may also need to re-open Xcode to get the new pods.
+            • Check that the library was linked correctly when you used the link command by running through the manual installation instructions in the README.
+            * If you are getting this error while unit testing you need to mock the native module. Follow the guide in the README.
+            If none of these fix the issue, please open an issue on the Github repository: https://github.com/telefon-one/react-native-tele`);
+            }
+
 
             NativeModules.TeleModule.start(configuration, (successful, data) => {
                 if (successful) {
